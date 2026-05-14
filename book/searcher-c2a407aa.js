c79d2a56da6bf59ebcb383831dae131bf5d93aaa
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2026-05-14 19:41:13 +0800

    chore(book): update build output after mdbook rebuild

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-25b7cd26.js',
+                path_to_root + 'searchindex-f7cc7386.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
