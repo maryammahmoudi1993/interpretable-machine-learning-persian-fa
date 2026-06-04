41ee9721df3be6c48327eb442a009a3b47602f95
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2026-06-04 18:20:15 +0800

    build(chapter-20): add built ALE chapter output

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6ebf1636.js',
+                path_to_root + 'searchindex-913a5b9b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
