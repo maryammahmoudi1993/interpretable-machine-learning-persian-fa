d1b16936d3be5435cb64288f602e0888a55419ff
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2026-06-06 12:10:04 +0800

    feat(chapter-23): add permutation feature importance chapter with images

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-956ca654.js',
+                path_to_root + 'searchindex-a89c413c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
