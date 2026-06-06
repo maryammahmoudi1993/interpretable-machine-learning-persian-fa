ca25e592ce80e80e5e5bbef2c73e76e3dbf017a9
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2026-06-06 14:17:57 +0800

    final commit

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1f933851.js',
+                path_to_root + 'searchindex-ee1d4605.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- src/chapters/13-ice.md
+++ src/chapters/13-ice.md
@@ -1,11 +1,1 @@
-# فصل ۱۳: انتظار شرطی فردی (ICE)
-
-> **عنوان اصلی:** Individual Conditional Expectation (ICE)  
-> **منبع:** [https://christophm.github.io/interpretable-ml-book/ice.html](https://christophm.github.io/interpretable-ml-book/ice.html)  
-> **نویسنده:** Christoph Molnar  
-> **مترجم:** مریم محمودی
-
----
-
-*این فصل هنوز ترجمه نشده است.*
-
+# ICE
