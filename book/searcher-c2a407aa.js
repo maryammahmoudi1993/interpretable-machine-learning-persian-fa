5ddea6288730f921efe1e41300ca09a93486c28d
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2026-06-06 13:51:11 +0800

    final commit

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f4b9358f.js',
+                path_to_root + 'searchindex-9e26a956.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
